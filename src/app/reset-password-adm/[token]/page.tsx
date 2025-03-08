73adf2bb58e3cf33a06f4e3798acedb4b4177cf8
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2025-03-08 19:26:27 -0300

    :bug: fix: corrigindo erro de tipagem

--- src/app/reset-password-adm/[token]/page.tsx
+++ src/app/reset-password-adm/[token]/page.tsx
@@ -1,15 +1,15 @@
 import ResetPasswordFormAdm from '@/components/reset-form-adm'
 
 interface ResetPasswordAdmProps {
-  params: {
+  params: Promise<{
     token: string
-  }
+  }>
 }
 
 export default async function ResetPasswordAdm({
   params,
 }: ResetPasswordAdmProps) {
-  const { token } = params
+  const { token } = await params
 
   return (
     <div className="mb-2 mt-4 flex min-h-screen flex-col items-center gap-5 pt-24 md:mt-0 md:pt-[165px]">
--- src/app/reset-password/[token]/page.tsx
+++ src/app/reset-password/[token]/page.tsx
@@ -1,15 +1,14 @@
 import ResetPasswordForm from '@/components/reset-form'
 
 interface ResetPasswordPageProps {
-  params: {
+  params: Promise<{
     token: string
-  }
+  }>
 }
-
 export default async function ResetPasswordPage({
   params,
 }: ResetPasswordPageProps) {
-  const { token } = params
+  const { token } = await params
 
   return (
     <div className="mb-2 mt-4 flex min-h-screen flex-col items-center gap-5 pt-24 md:mt-0 md:pt-[165px]">
