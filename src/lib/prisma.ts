a136d7d9ad6facde40f5d83af6d25ffb772dd6f5
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2025-04-16 23:18:17 -0300

    fix: correção no prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from '@/generated/prisma'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-export const prisma = new PrismaClient({
-  log: ['query'],
-})
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient
+}
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
