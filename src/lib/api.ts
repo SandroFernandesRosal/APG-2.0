aec246166faf93e0ac30f67d7702a4593398c126
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2025-04-18 20:23:51 -0300

    fix: correção

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-const isServer = typeof window === 'undefined'
-
 export const api = axios.create({
-  baseURL: isServer ? process.env.API_URL : process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 })
