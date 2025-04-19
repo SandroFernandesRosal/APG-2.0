ef3b6721bcfcfb935ee4c40a439ab4c57216e263
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2025-04-18 21:18:51 -0300

    fix: teste

--- src/app/quemsomos/page.tsx
+++ src/app/quemsomos/page.tsx
@@ -1,23 +1,7 @@
-import LiderQuemSomos from '@/components/LiderQuemSomos'
-import ContentQuemSomos from '@/components/ContentQuemSomos'
-import { SobreLider } from '@/data/types/sobrelider'
-import { Sobre } from '@/data/types/sobre'
-import QuemSomosHeader from '@/components/quemsomos-header'
-
-export default async function QuemSomos() {
-  const resSobre = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/sobre`)
-  const dataSobre = (await resSobre.json()) as Sobre[]
-
-  const resLider = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/sobrelider`)
-  const dataSobreLider = (await resLider.json()) as SobreLider[]
-
+export default function QuemSomos() {
   return (
     <main className="mb-2 flex min-h-screen flex-col items-center pt-20 md:mt-0 md:pt-[165px]">
-      <article className="mb-5 flex w-full flex-col items-center pb-5">
-        <QuemSomosHeader />
-        <LiderQuemSomos dataSobreLider={dataSobreLider} />
-        <ContentQuemSomos dataSobre={dataSobre} />
-      </article>
+      aaaaaaaa
     </main>
   )
 }
