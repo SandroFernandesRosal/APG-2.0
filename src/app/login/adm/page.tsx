f2f081abd4113852c35b61c2524b8c66b55c6981
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2025-04-21 00:01:34 -0300

    :sparkles: feat: verificação de admin ou membro logado

--- src/app/login/adm/page.tsx
+++ src/app/login/adm/page.tsx
@@ -1,14 +1,47 @@
 'use client'
+
 import Link from 'next/link'
-import { useState, FormEvent } from 'react'
+import { useState, useEffect, FormEvent } from 'react'
 import { useRouter } from 'next/navigation'
-import Cookies from 'js-cookie'
+import LogoutIgreja from '@/components/LogoutIgreja'
 
 export default function Register() {
   const [login, setLogin] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const [error, setError] = useState<string | boolean>(false)
+  const [isAdminLogged, setIsAdminLogged] = useState(false)
+  const [isMemberLogged, setIsMemberLogged] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+
   const router = useRouter()
+
+  useEffect(() => {
+    const checkLogins = async () => {
+      try {
+        const adminRes = await fetch('/api/auth/admin/login/me', {
+          method: 'GET',
+          credentials: 'include',
+        })
+        if (adminRes.ok) {
+          setIsAdminLogged(true)
+        }
+
+        const memberRes = await fetch('/api/auth/login/me', {
+          method: 'GET',
+          credentials: 'include',
+        })
+        if (memberRes.ok) {
+          setIsMemberLogged(true)
+        }
+      } catch (err) {
+        console.error('Erro ao verificar login:', err)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    checkLogins()
+  }, [])
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault()
@@ -20,6 +53,7 @@ export default function Register() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ login, password }),
+        credentials: 'include',
       })
 
       const user = await response.json()
@@ -29,32 +63,73 @@ export default function Register() {
         return
       }
 
-      if (response.status === 200 && user) {
-        Cookies.set('tokennn', user.token)
+      if (response.status === 200) {
         router.push('/')
         window.location.href = '/'
       }
     } catch {
-      setError('Erro ao redefinir a senha. Tente novamente mais tarde.')
+      setError('Erro ao fazer login. Tente novamente mais tarde.')
     }
+  }
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <p>Carregando...</p>
+      </div>
+    )
+  }
+
+  if (isMemberLogged) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+        <h1 className="text-2xl font-bold text-primary dark:text-secundary mb-4">
+          Você está logado como <span className="text-green-600">membro</span>.
+        </h1>
+        <p className="mb-2">
+          Faça logout como membro e entre como admin para continuar.
+        </p>
+        <div className="flex gap-2">
+          {' '}
+          <Link href="/" className="button !mb-0">
+            Voltar para Home
+          </Link>{' '}
+          ou
+          <LogoutIgreja />
+        </div>
+      </div>
+    )
+  }
+
+  if (isAdminLogged) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+        <h1 className="text-2xl font-bold text-primary dark:text-secundary mb-4">
+          Você já está logado como{' '}
+          <span className="text-red-500">administrador</span>.
+        </h1>
+        <Link href="/" className="button">
+          Voltar para a Home
+        </Link>
+      </div>
+    )
   }
 
   return (
     <div className="flex w-full justify-center min-h-screen pt-28 md:pt-[185px]">
       <div className="flex flex-col items-center border-[1px] border-zinc-400 dark:border-zinc-700 w-[70%] max-w-[500px] h-full rounded-md">
-        <h1 className="mt-2 text-lg font-bold text-primary dark:text-secundary">
-          Login Adm
+        <h1 className="mt-2 text-xl font-bold text-primary dark:text-secundary mb-5">
+          Login para Administrador
         </h1>
-        <p className="mb-5 text-xl">Use suas credenciais</p>
+
+        <Link className="button !mb-0" href={'/login/igreja'}>
+          Alterar para Membro
+        </Link>
 
         <form
           className="flex w-full flex-col items-center gap-3 rounded-xl p-3 md:mb-5"
           onSubmit={handleSubmit}
         >
-          <h1 className="text-xl font-bold text-primary dark:text-secundary">
-            Preencha os campos abaixo:
-          </h1>
-
           <p className="font-bold text-red-800">{error}</p>
 
           <input
--- src/app/login/igreja/page.tsx
+++ src/app/login/igreja/page.tsx
@@ -1,14 +1,49 @@
 'use client'
+
 import Link from 'next/link'
-import { useState, FormEvent } from 'react'
+import { useState, useEffect, FormEvent } from 'react'
 import { useRouter } from 'next/navigation'
-import Cookies from 'js-cookie'
+import Logout from '@/components/Logout'
 
 export default function RegisterIgreja() {
   const [login, setLogin] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const [error, setError] = useState<string | boolean>(false)
+  const [isAdminLogged, setIsAdminLogged] = useState(false)
+  const [isMemberLogged, setIsMemberLogged] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+
   const router = useRouter()
+
+  useEffect(() => {
+    const checkLogins = async () => {
+      try {
+        const memberRes = await fetch('/api/auth/login/me', {
+          method: 'GET',
+          credentials: 'include',
+        })
+
+        if (memberRes.ok) {
+          setIsMemberLogged(true)
+        }
+
+        const adminRes = await fetch('/api/auth/admin/login/me', {
+          method: 'GET',
+          credentials: 'include',
+        })
+
+        if (adminRes.ok) {
+          setIsAdminLogged(true)
+        }
+      } catch (err) {
+        console.error('Erro ao verificar login:', err)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    checkLogins()
+  }, [])
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault()
@@ -19,6 +54,7 @@ export default function RegisterIgreja() {
         headers: {
           'Content-Type': 'application/json',
         },
+        credentials: 'include',
         body: JSON.stringify({ login, password }),
       })
 
@@ -29,9 +65,7 @@ export default function RegisterIgreja() {
         return
       }
 
-      if (response.status === 200 && user) {
-        Cookies.set('tokenigreja', user.token)
-
+      if (response.status === 200) {
         router.push('/testemunhos')
         window.location.href = '/testemunhos'
       }
@@ -40,13 +74,60 @@ export default function RegisterIgreja() {
     }
   }
 
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <p>Carregando...</p>
+      </div>
+    )
+  }
+
+  if (isAdminLogged) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+        <h1 className="text-2xl font-bold text-primary dark:text-secundary mb-4">
+          Você está logado como{' '}
+          <span className="text-red-500">administrador</span>.
+        </h1>
+        <p className="mb-2">
+          Faça logout como admin e entre como membro para continuar.
+        </p>
+        <div className="flex gap-2">
+          {' '}
+          <Link href="/" className="button !mb-0">
+            Voltar para Home
+          </Link>{' '}
+          ou
+          <Logout />
+        </div>
+      </div>
+    )
+  }
+
+  if (isMemberLogged) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+        <h1 className="text-2xl font-bold text-primary dark:text-secundary mb-4">
+          Você já está logado como{' '}
+          <span className="text-green-600">membro</span>.
+        </h1>
+        <Link href="/" className="button">
+          Voltar para Home
+        </Link>
+      </div>
+    )
+  }
+
   return (
     <div className="flex w-full  justify-center  min-h-screen pt-28 md:pt-[185px]">
       <div className=" flex  flex-col items-center border-[1px] border-zinc-400 dark:border-zinc-700 w-[70%] max-w-[500px] h-full rounded-md ">
-        <h1 className="mt-2 text-lg font-bold text-primary dark:text-secundary ">
-          Login
+        <h1 className="mt-2 text-xl font-bold text-primary dark:text-secundary mb-5">
+          Login para membros
         </h1>
-        <p className="mb-5 text-xl">Use suas credenciais</p>
+
+        <Link className="button !mb-0" href={'/login/adm'}>
+          Alterar para Administrador
+        </Link>
 
         <form
           className="flex w-full flex-col items-center gap-3 rounded-xl  p-3  md:mb-5"
